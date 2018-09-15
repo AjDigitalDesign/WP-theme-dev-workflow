380d01eac48361be3487cb1d9a67baedd4f1502c
Aj <alfred@Alfreds-iMac.local>
2018-09-15 09:36:48 -0400

    added gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,43 @@ var themename = 'underscoresass';
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var jshint = require('gulp-jshint');
+
 
 
 //name of working theme folder
 root = '../' + themename + '/',
-scss = root + 'sass/'
+scss = root + 'sass/',
+js = root + 'assets/js/'
+
+//Dstination file for production
+jsproduction = root + 'assets/production/js'
+
 
 
 
 gulp.task('sass', function(){
      return gulp.src(scss + '{style.scss, rtl.scss}')
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'expandded'}).on('error', sass.logError))
+        .pipe(postcss([
+            autoprefixer('last 2 versions', '> 1%')
+        ]))
+        .pipe(sourcemaps.write(scss + 'maps'))
         .pipe(gulp.dest(root));
+});
+
+
+gulp.task('scripts', function(){
+    return gulp.src([js + '*.js'])
+    .pipe(concat('min.js'))
+    .pipe(gulp.dest(jsproduction))
 });
 
 gulp.task('watch', function(){
@@ -24,7 +47,9 @@ gulp.task('watch', function(){
         proxy: 'http://wordpressdev.test/',
         port: 8080
     });
-    gulp.watch([root + '**/*.css', root + '**/*.scss'], ['css']);
+    gulp.watch([root + '**/*.css', root + '**/*.scss'], ['sass']);
+    gulp.watch(js + '**/*.js', ['scripts']);
+    gulp.watch(root + '**/*').on('change', browserSync.reload);
 
 
 })
